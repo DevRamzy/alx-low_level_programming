ba674feac95088eeac3414dfb4229d69134c95b2
Ramzy254 <fmwangi811@gmail.com>
2021-06-17 18:46:50 +0300

    Challenge 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main () {
+   char str1[50];
+
+   strcpy(str1, "Programming is like building a multilingual puzzle");
+
+   puts(str1);
+   
+   return(0);
+}
