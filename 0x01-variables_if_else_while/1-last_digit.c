148a92fa5c5700f72f9dd5828d917ae067dec72b
Ramzy254 <fmwangi811@gmail.com>
2021-06-20 14:22:54 +0300

    Challenge 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,14 +1,19 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+int n, lastDigit;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+/* your code goes there */
+lastDigit = n % 10;
+printf("Last digit = %d", lastDigit);
+return (0);
 }
