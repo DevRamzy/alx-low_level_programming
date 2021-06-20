7472a97d06d0bdf8f41e1d71211c00511cbc8433
Ramzy254 <fmwangi811@gmail.com>
2021-06-20 12:27:45 +0300

    Challenge 6

--- 0x00-hello_world/100-main.c
+++ 0x00-hello_world/100-main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-
-int main(void) {
-    printf("Hello World");
-    return 0;
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+return (0);
 }
