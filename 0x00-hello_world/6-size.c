9ad4350e95f3175e4dda206a3b4a119df951ebad
Ramzy254 <fmwangi811@gmail.com>
2021-06-20 12:01:17 +0300

    Challenge 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main(void)
     float floatType;    
 
     /* sizeof evaluates the size of a variable */
-    printf("Size of char: %zu byte(s)\n", sizeof(charType));
+    printf("Size of a char: %zu byte(s)\n", sizeof(charType));
     printf("Size of an int: %zu byte(s)\n", sizeof(intType));
     printf("Size of a long int: %zu byte(s)\n", sizeof(longIntType));
     printf("Size of a long long int: %zu byte(s)\n", sizeof(longLongIntType));
