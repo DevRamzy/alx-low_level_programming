d6b1a0dccc9632f5ed7a89b046a4221478541bf6
Ramzy254 <fmwangi811@gmail.com>
2021-06-20 15:58:41 +0300

    Challenge 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 /* more headers goes there */
-
-/* betty style doc for function main goes there */
+/* more headers goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char ch = 'a';
 while (ch <= 'z')
 {
-printf("%c ", ch);
+printf("%c", ch);
 ch++;
 }
 printf("\n");
-return 0;  
+return (0);  
 }
