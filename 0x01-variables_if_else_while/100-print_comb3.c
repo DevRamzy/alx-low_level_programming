749b7964604f844da63fc15fa90e414c4001648b
Ramzy254 <fmwangi811@gmail.com>
2021-06-21 14:04:22 +0300

    Challanges 8,9, 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,5 +10,23 @@
  */
 int main(void)
 {
+int i, j;
+for (i = '0'; i <= '9'; i++)
+{
+for (j = '0'; j <= '9'; j++)
+{
+if (j > i)
+{
+if (!(i == '0' && j == '1'))
+{
+putchar(',');
+putchar(' ');
+}
+putchar(i);
+putchar(j);
+}
+}
+}
+putchar('\n');
 return (0);
 }
--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,7 +11,11 @@
 int main(void)
 {
 int i;
-for (i = 48; i <= 66; i++)
+for (i = '0'; i <= '9'; i++)
+{
+putchar(i);
+}
+for (i = 'a'; i <= 'f'; i++)
 {
 putchar(i);
 }
